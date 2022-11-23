98a5edd152f7248cbc10bcec5a0b86cce15f47e7
Mahindra Finance DFB IT <mahindrafinancedfbit@gmail.com>
2022-11-23 14:43:10 +0530

    Detail page cr

--- src/app/scheme-detail/scheme-detail.component.ts
+++ src/app/scheme-detail/scheme-detail.component.ts
@@ -1,29 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ApiService } from '../service/api.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Store } from '@ngrx/store';
-import { AppData } from 'src/app/models/app';
-import { NgModule } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from '../service/api.service';
+import { AppData } from '../models/app';
 import { LoaderComponent } from '../loader/loader.component';
-import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from '../service/auth.service';
+
 @Component({
   selector: 'app-scheme-detail',
   templateUrl: './scheme-detail.component.html',
   styleUrls: ['./scheme-detail.component.css']
 })
 export class SchemeDetailComponent implements OnInit {
-  schemeAmount="";
-  data: any;
-  isShowDivIf = false; 
-  formula : any
-  isrange=false
+  schemeAmount:string = "";
+  data:any;
+  isShowDivIf:boolean = false; 
+  formula:any
+  isrange:boolean = false;
   appData$: Observable<AppData>;
   appData: AppData = {}
-  rateInput: any;
+
   constructor(
     public http: HttpClient,
     public apiService: ApiService,
@@ -49,49 +48,42 @@ export class SchemeDetailComponent implements OnInit {
       backdrop:'static',
       keyboard: false,
       windowClass:"remove-bg-modal",
-      size:"sm",
-    // modalDialogClass: " modal-dialog-centered d-flex justify-content-center"
+      size:"sm"
     });
     var id = this.router.snapshot.params['id'];
-    let token:String = ''; 
+    let token: String = ''; 
     if(this.appData.token){
       token = this.appData.token;
     }
     var sessionData = this.authService.getData();
     var obj = {
       'posid': sessionData?.salesExecutive?.posId,
-      'id':id
+      'id': id
     };
     this.apiService.dcgById(obj, token).subscribe((data: any) => {
       loaderRef.close();
-      console.log('data: ', data);
       this.data = data;
     }, (error: any) => console.error(error));
   }
 
   validateDetails (amtamount: any) {
-    // console.log('  this.data[0].min_amount: ',   this.data[0].min_amount);
-    // console.log('this.data[0].max_amount : ', this.data[0].max_amount );
     this.isrange = false;
-    if( Number(amtamount) < Number(this.data[0].min_amount) ){
+    if (Number(amtamount) < Number(this.data[0].min_amount)) {
       this.isrange = true;
     } else if (Number(amtamount) > Number (this.data[0].max_amount)) {
       this.isrange = true;
-    }
-    else{
-      this.isrange=false;
+    } else {
+      this.isrange = false;
       const loaderRef = this.modalService.open(LoaderComponent,{
         centered: true,
         animation:true,
         backdrop:'static',
         keyboard: false,
         windowClass:"remove-bg-modal",
-      size:"sm",
-      // modalDialogClass: " modal-dialog-centered d-flex justify-content-center"
+        size:"sm"
       });
-      this.isShowDivIf=true
+      this.isShowDivIf = true;
       var loan_amount=Number(amtamount);
-      console.log('loan_amount: ', loan_amount);
       var id= this.router.snapshot.params['id'];
       let token:String = ''; 
       if(this.appData.token){
@@ -101,64 +93,43 @@ export class SchemeDetailComponent implements OnInit {
         'posid':  this.authService.userPosId,
         'id': id
       };
-      // this.apiService.dcgById(obj, token).subscribe((data: any) => {
-        var data = this.data;
-        // console.log('data: ', data);
-          loaderRef.close();
-        var tenure=Number(data[0].tenure)
+      var data = this.data;
+      loaderRef.close();
+      var tenure=Number(data[0].tenure);
       var rate = data[0].roi 
       let result = rate.split('%')[0];
-      var newrate=result/100;
-      // newrate=Math.round(newrate*100)/100;
-      var EMIAmount= Number((loan_amount*newrate/12)/(1-Math.pow((1+newrate/12),(-tenure))))
-      EMIAmount=Math.round(EMIAmount*100)/100;
-      console.log('EMIAmount: ',(Math.round), EMIAmount);
-      var ADVANCE_EMI=  Number(EMIAmount * data[0].advance_emi)
-      ADVANCE_EMI=Math.round(ADVANCE_EMI*100)/100;
-      console.log('ADVANCE_EMI: ', ADVANCE_EMI);
-      var PROCESSING_FEE =Number(data[0].processing_fee) 
-      var  TotalPaymenttransaction=Number ( ADVANCE_EMI +PROCESSING_FEE)
-      TotalPaymenttransaction=Math.round(TotalPaymenttransaction*100)/100;
-      // console.log('data.PROCESSING_FEE: ', PROCESSING_FEE);
-      var remaningemi=data[0].tenure - data[0].advance_emi
-      console.log('TotalPaymenttransaction: ', TotalPaymenttransaction);
-      var TotalIntrestduringloantenure =Number((EMIAmount * data[0].tenure) - loan_amount)
-      TotalIntrestduringloantenure=Math.round(TotalIntrestduringloantenure*100)/100;
-      console.log('TotalIntrestduringloantenure: ', TotalIntrestduringloantenure);
-      var Totalofintrest = Number (TotalIntrestduringloantenure + PROCESSING_FEE)
-      Totalofintrest=Math.round(Totalofintrest*100)/100;
-      console.log('Totalofintrest: ', Totalofintrest);
-      var Advance_EMI = ADVANCE_EMI
-      console.log('Advance_EMI: ', Advance_EMI);
-      var Amounttobepaidby =TotalPaymenttransaction
-      console.log('Amounttobepaidby: ', Amounttobepaidby);
-      var additional_Cashback=0;
-      var net_Cost_Custmerr= Totalofintrest- TotalIntrestduringloantenure - additional_Cashback
-      // net_Cost_Custmer=Math.round(net_Cost_Custmer*100)/100;
-      var net_Cost_Custmer = net_Cost_Custmerr.toFixed(2)
-      var dbd_result = data[0].dbd
+      var newrate = result/100;
+      var emiAmount = Number((loan_amount*newrate/12)/(1-Math.pow((1+newrate/12),(-tenure))));
+      emiAmount = Math.round(emiAmount*100)/100;
+      var advanceEmi=  Number(emiAmount * data[0].advance_emi);
+      advanceEmi = Math.round(advanceEmi*100)/100;
+      var processing_fee = Number(data[0].processing_fee);
+      var transactionPayment = Number ( advanceEmi + processing_fee);
+      transactionPayment = Math.round(transactionPayment*100)/100;
+      var remaningemi = data[0].tenure - data[0].advance_emi;
+      var totalInterest = Number((emiAmount * data[0].tenure) - loan_amount);
+      totalInterest = Math.round(totalInterest*100)/100;
+      var totalOfInterest = Number(totalInterest + processing_fee);
+      totalOfInterest=Math.round(totalOfInterest*100)/100;
+      var additional_Cashback = 0;
+      var net_Cost_Custmerr= totalOfInterest- totalInterest - additional_Cashback;
+      var net_Cost_Custmer = net_Cost_Custmerr.toFixed(2);
+      var dbd_result = data[0].dbd;
       let dbdresult = dbd_result.split('%')[0];
-      console.log('dbdresult: ', dbdresult);
       var dbdrate=Number(dbdresult/100);
-      console.log('dbd_result: ', dbdrate);
-      var dbd = dbdrate * amtamount
-      var distributed_delerto_mmfsl= amtamount-(dbd + PROCESSING_FEE + Advance_EMI)
-      this.formula={ EMIAmount:EMIAmount ,remaningemi:remaningemi
-        ,ADVANCE_EMI:ADVANCE_EMI,
-        Totalofintrest:Totalofintrest,
-        Amounttobepaidby:Amounttobepaidby,
-        TotalIntrestduringloantenure:TotalIntrestduringloantenure,
-        net_Cost_Custmer:net_Cost_Custmer,
-        dbd:dbd,
-        distributed_delerto_mmfsl:distributed_delerto_mmfsl};
-      console.log('formula: ', this.formula);
-      // }, (error: any) => console.error(error));
-    
+      var dbd = dbdrate * amtamount;
+      var distributed_delerto_mmfsl = amtamount-(dbd + processing_fee + advanceEmi);
+      this.formula = {
+        emiAmount: emiAmount,
+        remaningemi: remaningemi,
+        advance_emi: advanceEmi,
+        totalOfInterest: totalOfInterest,
+        amountPaidBy: transactionPayment,
+        totalInterest: totalInterest,
+        net_Cost_Custmer: net_Cost_Custmer,
+        dbd: dbd,
+        distributed_delerto_mmfsl: distributed_delerto_mmfsl
+      };
     }
-  
-  }
-  range(){
-  //  this.amtamount;
-
   }
 }
